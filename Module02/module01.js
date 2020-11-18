e269724733508f4483f497f87c1b3bafef99b626
Saradha-Devi <74523240+Saradha-Devi@users.noreply.github.com>
2020-11-18 09:56:10 +0530

    Add files via upload

---
+++ Module02/module01.js
@@ -0,0 +1,9 @@
+import { LightningElement } from 'lwc';
+
+export default class Module01 extends LightningElement {
+    a= 'Saradha';
+    //eslint-disable-next-line no-undef
+    debugger;
+    a = 'Devi';
+    debugger;
+}
